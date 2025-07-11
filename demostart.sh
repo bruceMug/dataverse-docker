b9d4836b620e95c7416e8d294ac6c85869b7010d
brucemug <mugzbru199@gmail.com>
2025-07-11 17:30:35 +0300

    new script for starting containers

--- demostart.sh
+++ demostart.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-# docker network create proxy_hostinger-network
-# cp .env_sample .env
-docker-compose up -d
+
+# Bring down any running containers
+docker-compose down
+
+# Start postgres, minio, and whoami in detached mode
+docker-compose up -d postgres minio whoami
+
+# Wait for these services to be ready (simple sleep, consider health checks)
+sleep 15
+
+# Start solr
+docker-compose up -d solr
+
+# Wait for solr to be ready
+sleep 15
+
+# Start dataverse
+docker-compose up -d dataverse
